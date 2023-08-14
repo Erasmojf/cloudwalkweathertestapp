47a4cf6e006aa6cdac566d781eb9a801a1b5f52b
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2023-08-14 09:56:27 -0300

    Refactoring weathet _repository

--- weathertestapp/lib/repositories/weather_repository.dart
+++ weathertestapp/lib/repositories/weather_repository.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:weathertestapp/models/networh_state.dart';
+import 'package:weathertestapp/models/network_state.dart';
 
 abstract class WeatherService {
   Future findByCityName(String cityName);
